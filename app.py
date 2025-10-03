fcfed578f1b835856ac2f77f7b9130c5908c391f
Gopi Battineni <117643410+gopibattineni@users.noreply.github.com>
2025-10-03 13:08:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def extract_images_from_pdf(pdf_file):
 st.markdown(""" <div style="display:flex; justify-content:space-between; align-items:center;">
         <img src="CIRM.png"> <img src="logo.png"> </div> """, unsafe_allow_html=True)
 
-st.markdown('<h1 class="centered">CIRM Foundation & CIRM SERVIZI Srl</h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="centered">CIRM Foundation & CIRM SERVIZI</h1>', unsafe_allow_html=True)
 st.markdown('<h3 class="centered">🧑‍⚕️ MarineDerma - Skin Disease Detection App</h3>', unsafe_allow_html=True)
 st.write("Upload an image or PDF report for prediction.")
 
